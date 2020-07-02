8f79efa394d0c3de7e9f33456bc5827b859bf624
dsupriya <supriyad570@gmail.com>
2020-07-02 23:23:44 +0530

    Create Reverse a Linked List in groups of given size.java

---
+++ linked-list/Reverse a Linked List in groups of given size.java
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            GfG gfg = new GfG();
+            Node res = gfg.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class GfG
+{
+    public static Node reverse(Node head, int k)
+    {
+       Node cur, temp,prev;
+       temp=prev=null;
+       cur = head;
+       int c =0;
+       while(c<k&& cur!=null)
+       {
+           temp = cur.next;
+           cur.next = prev;
+           prev = cur;
+           cur = temp;
+           c++;
+       }
+       if(temp!=null)
+            head.next = reverse(temp, k);
+        return prev;
+        
+    }
+}
+
