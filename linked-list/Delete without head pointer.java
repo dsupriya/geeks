5a97ebb99b0adb2375290f5e3968d8273c00c519
dsupriya <supriyad570@gmail.com>
2020-07-04 14:25:11 +0530

    Create Delete without head pointer.java

---
+++ linked-list/Delete without head pointer.java
@@ -0,0 +1,143 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Delete_Node
+{
+	Node head;
+	Node tail;
+	
+	void printList(Node head)
+	{
+		Node tnode = head;
+		while(tnode != null)
+		{
+			System.out.print(tnode.data+ " ");
+			tnode = tnode.next;
+		}
+		System.out.println();
+	}
+	
+	void addToTheLast(Node node)
+	{
+		
+		if(head == null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+	}
+	
+	Node search_Node(Node head, int k)
+	{
+		Node current = head;
+		while(current != null)
+		{
+			if(current.data == k)
+				break;
+			current = current.next;
+		}
+	return current;
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			Delete_Node llist = new Delete_Node();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+			int k = sc.nextInt();
+			Node del = llist.search_Node(llist.head,k);
+			
+			GfG g = new GfG();
+			if(del != null && del.next != null)
+			{
+				g.deleteNode(del);
+			}
+			llist.printList(llist.head);
+		t--;
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+
+// This function should delete node from linked list. The function
+// may assume that node exists in linked list and is not last node
+// node: reference to the node which is to be deleted
+class GfG
+{
+    void deleteNode(Node head)
+    {
+         // Your code here
+         Node cur = head;
+         Node prev =head;
+         int t=0;
+         /*while(cur!=null && cur.next!=null)
+         {
+             t = cur.data;
+             cur.data = cur.next.data;
+             cur.next.data = t;
+             prev = cur;
+             cur= cur.next;
+         }*/
+         
+         //prev.next = null;
+        t = cur.data;
+        cur.data = cur.next.data;
+        cur.next.data = t;
+        
+        if(cur.next!=null)
+        {
+            cur.next = cur.next.next;
+        }
+        else
+        {
+            cur.next = null;
+        }
+    }
+}
+
